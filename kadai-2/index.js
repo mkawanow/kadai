ad5593393916f41c5a80a7519f677f3b5c542f25
mkawanow <mkawa.now@gmail.com>
2019-10-23 05:00:46 +0000

    Add a program

---
+++ kadai-2/index.js
@@ -0,0 +1,13 @@
+const firstname = 'hideyo'
+const secondname = 'noguchi'
+console.log(firstname+secondname)
+
+const sum = 1+2+3+4+5+6+7+8+9+10
+console.log(sum)
+
+const hasdog = true
+const hascat = false
+
+console.log(hasdog || hascat)
+console.log(hasdog && hascat)
+console.log(!hasdog && !hascat)
